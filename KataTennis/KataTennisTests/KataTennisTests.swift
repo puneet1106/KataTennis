ef6a02caccb72c2bffe42334c3408464c8ae24a4
Puneet Rangnekar <puneet.rangnekar@goplayfora.com>
2023-02-17 12:42:52 +0530

    TestCase For Second Third points

--- KataTennis/KataTennisTests/KataTennisTests.swift
+++ KataTennis/KataTennisTests/KataTennisTests.swift
@@ -26,4 +26,19 @@ class KataTennisTests: XCTestCase {
         let score = gameScore.getPlayerPoints(player1Points: 1, player2Points: 1)
         XCTAssertEqual("Fifteen All", score)
     }
+    
+    func test_gameScore_WhenPlayer1ScoreSecondPoint_Player2ScoreFirstPoint() {
+        let score = gameScore.getPlayerPoints(player1Points: 2, player2Points: 1)
+        XCTAssertEqual("Thirty - Fifteen", score)
+    }
+ 
+    func test_gameScore_WhenPlayer1ScoreSecondPoint_Player2ScoreSecondPoint() {
+        let score = gameScore.getPlayerPoints(player1Points: 2, player2Points: 2)
+        XCTAssertEqual("Thirty All", score)
+    }
+
+    func test_gameScore_WhenPlayer1ScoreThirdPoint_Player2ScoreSecondPoint() {
+        let score = gameScore.getPlayerPoints(player1Points: 3, player2Points: 2)
+        XCTAssertEqual("Forty - Thirty", score)
+    }
 }
